02982a0d26e586447e713ed22f999272417318a7
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-05-26 22:06:52 -0500

    Limpiando un poco

--- Ghost Local/versions/1.22.7/core/server/lib/mobiledoc/cards/html.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'html',
-    type: 'dom',
-    render(opts) {
-        // use the SimpleDOM document to create a raw HTML section.
-        // avoids parsing/rendering of potentially broken or unsupported HTML
-        return opts.env.dom.createRawHTMLSection(opts.payload.html);
-    }
-};
