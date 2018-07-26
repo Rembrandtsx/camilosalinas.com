7561572e065aab9b5a125dd0efcedb1c59afb242
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-07-26 06:06:03 -0500

    Presentación intro a la web

--- frontEnd/PWA/src/app/pipes/exists.pipe.ts
+++ frontEnd/PWA/src/app/pipes/exists.pipe.ts
@@ -6,6 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
   export class ExistsPipe implements PipeTransform {
     transform(value: String): String {
         
-        return value == ' '|| value == " " || value == null || value== undefined? 'display-none':'';
+        return value == ''|| value == "" ||value == ' '|| value == " " || value == null || value== undefined? 'display-none':'';
     }
   }
