27daa2dbc8307cc93869b46cfb2ef9feaa7fde30
Camilo Salinas <camilo@ggitapps.com>
2018-11-19 06:23:29 -0500

    Update home.component.ts

--- frontEnd/PWA/src/app/home/home.component.ts
+++ frontEnd/PWA/src/app/home/home.component.ts
@@ -33,7 +33,7 @@ export class HomeComponent implements OnInit {
 
     //Elemento Typed
     let typed:any = new Typed('#typed',{
-      strings:["Desarrollador","Estudiante","中文演讲人","Innovador","Monitor", "Ingeniero","Emprendedor", "Inventor"],
+      strings:["Developer","Student","中文演讲人","Creator","Monitor", "Engineer","Entrepeneur", "Inventor", "Student Representative"],
       backSpeed: 40,
       typeSpeed: 40,
       loop: true
