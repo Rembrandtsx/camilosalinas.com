379ba343d4045bc5334bae50d43eb3a5b3e911d4
Camilo Salinas <iam@camilosalinas.me>
2022-02-13 23:48:22 -0500

    Audioplayer

---
+++ audioplayer/lib/log.js
@@ -0,0 +1,15 @@
+const font = 'font-family: "NotoSans", "Helvetica", sans-serif;';
+
+const artist = 'Mø y Aurora';
+
+module.exports = function (msg) {
+  console.log(`%c${msg}`, font);
+};
+
+module.exports.intro = function (msg) {
+
+};
+
+module.exports.easterEgg = function () {
+
+};
